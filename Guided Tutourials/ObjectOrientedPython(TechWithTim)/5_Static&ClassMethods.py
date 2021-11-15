3c12416f1624ddcdf65a399fc2c4fb5eaab1fefa
stevo-beabo <72895504+stevo-beabo@users.noreply.github.com>
2021-11-15 12:20:59 +0100

    OOP Tutorial Completed

--- Guided Tutourials/ObjectOrientedPython(TechWithTim)/5_Static&ClassMethods.py
+++ Guided Tutourials/ObjectOrientedPython(TechWithTim)/5_Static&ClassMethods.py
@@ -1,20 +1,31 @@
 ## Video 5 of Object Oriented Programming with Python (https://www.youtube.com/watch?v=MpuOuZKWUWw)
 
 class Dog:
-	dogs = []
+	dogs = [] # Class variable, able to be called without making an instance of the class
 
 	def __init__(self, name):
 		self.name = name
-		self.dogs.append(self)
+		self.dogs.append(self) # Appends every dog object into the the list variable "dogs"
 
-	@classmethod
-	def num_dogs(cls):
+	@classmethod # Method Decorator to indicate the special type of method. Class Methods can be called without making an instance of the class
+	def num_dogs(cls): # Class Methods use the parameter "cls" instead of "self"
 		return len(cls.dogs)
 
-	@staticmethod
+	@staticmethod # Method Decorator to indicate the special type of method. Static Methods do not need the class or an instance of a class to be called. Cannot be refernced inside of the class either.
 	def bark(n): # Barks "n" amounts of times
 		for _ in range(n):
 			print("Bark!")
 
-tim = Dog("Tim")
-jim = Dog("Jim")
+
+class Math:
+	@staticmethod
+	def add(x, x2):
+		return x + x2
+
+#tim = Dog("Tim")
+#jim = Dog("Jim")
+
+print(Dog.dogs) # Prints out the value of the class variable 
+print(Dog.num_dogs()) # Prints out the results of the "num_dogs" method without creating an instance object first
+Dog.bark(5) # Use of the Static Method "bark" in the class Dog
+print(Math.add(5, 5)) # Using the Static Method "add" in the class Math
---
+++ Guided Tutourials/ObjectOrientedPython(TechWithTim)/mod.py
@@ -0,0 +1,1 @@
+6_PublicPrivateClasses_mod.py
